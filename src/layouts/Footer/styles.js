619317a7d9f64ba9b1b20e71fef214a5be73cdcf
Bruno Faria <bruno.faria.san@gmail.com>
2023-02-14 19:46:52 -0300

    feature: footer section construction.

---
+++ src/layouts/Footer/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 194px;
+  background: #f7b100;
+  filter: brightness(90%);
+  display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+export const Content = styled.div`
+  
+`;
